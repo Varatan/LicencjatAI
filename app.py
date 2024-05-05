33187fd15157f5ec66b1c7c479112c7e5902e77f
Varatan <varatan03@gmail.com>
2024-05-05 13:10:48 +0200

    git c

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-
-
-
-
 from flask import Flask, render_template
+# from openai import OpenAI
 
 app = Flask(__name__)
 
@@ -10,5 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/generate/")
+def make_turn():
+    return render_template("index.html")
+
+
+
 
 app.run(debug=True)
+
+# client = OpenAI()
+
+# response = client.chat.completions.create(
+#   model="gpt-3.5-turbo-0125",
+#   response_format={ "type": "json_object" },
+#   messages=[
+#     {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+#     {"role": "user", "content": "Who won the world series in 2020?"}
+#   ]
+# )
+# print(response.choices[0].message.content)
